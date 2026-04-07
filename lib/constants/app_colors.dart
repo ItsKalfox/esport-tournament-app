1d8e918f82cee8e1ce8fb45fa1d0d0a0e8371437
apsaraTD <thamodiapsara@gmail.com>
2026-04-07 11:00:24 +0530

    feat: add AppColors constants for home page theme

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background       = Color(0xFF1A1A1A);
+  static const Color cardBackground   = Color(0xFF2A2A2A);
+  static const Color primaryOrange    = Color(0xFFE07B2A);
+  static const Color darkOrange       = Color(0xFFB85E1A);
+  static const Color white            = Color(0xFFFFFFFF);
+  static const Color grey             = Color(0xFFAAAAAA);
+  static const Color darkGrey         = Color(0xFF3A3A3A);
+  static const Color navBarBackground = Color(0xFF111111);
+}
